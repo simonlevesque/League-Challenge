5a5f3c2f14078b9310b8a909129720cecc32b9b2
Simon <simon@pop-os.localdomain>
2022-09-11 13:07:00 -0400

    Relocate echo handler test in a common endpoint test file
    Also, separate the common error tests from the endpoint happy path tests

---
+++ backend/server/endpoint_test.go
@@ -0,0 +1,42 @@
+package server
+
+import (
+        "bytes"
+        "league/challenge/backend/matrix"
+        "mime/multipart"
+        "net/http"
+        "net/http/httptest"
+        "testing"
+)
+
+func TestEchoHandler(t *testing.T) {
+
+        body := bytes.NewBuffer(nil)
+        writer := multipart.NewWriter(body)
+
+        part, err := writer.CreateFormFile("file", "test-matrix.csv")
+        part.Write([]byte("1,2,3\n4,5,6\n7,8,9"))
+        writer.Close()
+
+        req, err := http.NewRequest("POST", "/echo", body)
+	if err != nil {
+		t.Fatal(err)
+	}
+        req.Header.Set("Content-Type", writer.FormDataContentType())
+
+        rec := httptest.NewRecorder()
+
+        handler := handleCsvMatrix(matrix.FmtSquareMatrix)
+	handler.ServeHTTP(rec, req)
+
+	if status := rec.Code; status != http.StatusOK {
+		t.Errorf("Echo handler returned wrong status code: expected %v actual %v",
+			http.StatusOK, status)
+	}
+
+        expected := "1,2,3\n4,5,6\n7,8,9\n"
+	if rec.Body.String() != expected {
+		t.Errorf("Echo handler returned wrong body: expected %v actual %v",
+			expected, rec.Body.String())
+	}
+}
